7284e251e438f49630c2c245af924c46608349c0
phsycodeid <phsycode.id@gmail.com>
2019-11-23 22:05:19 +0700

    monsterkill

--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -11,7 +11,7 @@ module.exports = {
     result.result = data
     return res.status(result.status).json(result)
   },
-  validateLogin: (req, reqData, passData) => {
+  validateLogin: (req, res, reqData, passData) => {
     if (passData.length != 0) {
       const reqPass = reqData.password
       const sqlPass = passData[0].password
@@ -19,7 +19,7 @@ module.exports = {
       const jwtheader = req.headers.jwt
       if (bcrypt.compareSync(reqPass, sqlPass)) {
         if (dbjwt === null) {
-          return 'Your token empty, pls REGISTER!'
+          res.json({token: 'Your Token doesnt exist'})
         } else {
           if (dbjwt === jwtheader) {
             const pyload = {
@@ -28,9 +28,9 @@ module.exports = {
             }
             const token = jwt.sign(pyload, process.env.KEYS, { expiresIn: '24h' })
             userModel.patchJwtById(token, pyload.id)
-            return token
+             res.json({token: token})
           } else {
-            return 'Your token isnt autorized'
+            res.json({ token : 'Your Token isnt autorized'})
           }
         }
       } else {
@@ -48,7 +48,7 @@ module.exports = {
     // return res.send(userId.toString())
 
     if (!token) {
-      return res.status(200).send('login dulu')
+      res.json({ token : 'Please Login to continue your step'})
     }
 
     try {
@@ -56,22 +56,22 @@ module.exports = {
       const tokenCheck = tokenDb[0].jwt
 
       if (!tokenDb && !token) {
-        return res.send('Login dulu')
+        res.json({ token : 'Please Login to continue your step'})    
       }
 
       if (tokenCheck === token) {
         jwt.verify(token, process.env.KEYS, (err, decode) => {
           if (tokenCheck === !token) {
-            return res.status(400).send('Token doesnt exist')
+            res.json({ token : 'Token Doesnt exist'})
           } else {
             next()
           }
         })
       } else {
-        return res.status(400).send('Please Login to Continue your step')
+        res.json({ token : 'Please Login to continue your step'})
       }
     } catch (error) {
-      return res.status(400).send('login dulu')
+      res.json({ token : 'Please Login to continue your step'})
     }
   }
 }
